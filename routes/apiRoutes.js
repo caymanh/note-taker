9615242155dd8adcee25da217ae6e6c3cdd9cc5b
Cayman Heng <hengcayman@gmail.com>
2020-12-14 11:36:19 -0500

    added api.delete method to handle delete requests. updated readFile and writeFile functions to fs.readFileSync and fs.writeFileSync to resolve syntax error when handling delete requests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,21 +10,32 @@ let notesArr = [];
 
 module.exports = (app) => {
   //Function to read file
+  // const readFile = () => {
+  //   fs.readFile("./db/db.json", "utf8", (err, data) => {
+  //     if (err) throw err;
+  //     notesArr = JSON.parse(data);
+  //     return notesArr;
+  //   });
+  // };
+
   const readFile = () => {
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-      notesArr = JSON.parse(data);
-      return notesArr;
-    });
+    const data = fs.readFileSync("./db/db.json", "utf8");
+    notesArr = JSON.parse(data);
+    return notesArr;
   };
 
   //Function to write file
+  // const writeFile = () => {
+  //   fs.writeFile("db/db.json", JSON.stringify(notesArr), (err) => {
+  //     if (err) throw err;
+  //     return true;
+  //   });
+  // };
+
   const writeFile = () => {
-    fs.writeFile("db/db.json", JSON.stringify(notesArr), (err) => {
-      if (err) throw err;
-      return true;
-    });
+    fs.writeFileSync("db/db.json", JSON.stringify(notesArr));
   };
+
   // API GET Requests
   // Below code handles when users "visit" a page.
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the note)
@@ -51,7 +62,12 @@ module.exports = (app) => {
     res.json(newNote);
     return console.log(`Added new note: ${newNote.title}`);
   });
-};
 
-//API DELETE Requests
-// app.delete("/api/notes/:id", (req, res) => {});
+  //API DELETE Requests
+  app.delete("/api/notes/:id", (req, res) => {
+    readFile();
+    notesArr = notesArr.filter((note) => note.id !== req.params.id);
+    writeFile();
+    res.json(notesArr);
+  });
+};
