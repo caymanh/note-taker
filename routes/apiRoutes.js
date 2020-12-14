9fb37d5c23bc4063b603e0db7249a7b3a7cd766b
Cayman Heng <hengcayman@gmail.com>
2020-12-14 10:22:34 -0500

    updated comments in the apiRoutes.js file to describe what does the API POST request do

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,32 +24,11 @@ module.exports = (app) => {
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
+  // When a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // (ex. User fills out a note... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-
-  //   app.post("/api/notes", (req, res) => {
-  //     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  //     // It will do this by sending out the value "true" have a table
-  //     // req.body is available since we're using the body parsing middleware
-  //     if (tableData.length < 5) {
-  //       tableData.push(req.body);
-  //       res.json(true);
-  //     } else {
-  //       waitListData.push(req.body);
-  //       res.json(false);
-  //     }
-  //   });
-
-  // app.post("/api/notes", (req, res) => {
-  //   uniqid();
-  //   const newNote = req.body;
-  //   notesArr.push(newNote);
-  //   res.json(newNote);
-  //   console.log(notesArr);
-  // });
 
   app.post("/api/notes", (req, res) => {
     uniqid();
