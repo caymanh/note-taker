f9f428e273fa5e6b2297f8b48ee68e221b97c93b
Cayman Heng <hengcayman@gmail.com>
2020-12-14 12:26:56 -0500

    moved index.html file to root directory and updated db.json file to an empty array

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,6 +16,6 @@ module.exports = (app) => {
 
   // If no matching route is found default to home
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../index.html"));
   });
 };
